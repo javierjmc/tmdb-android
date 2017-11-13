aa7015d2b32f0aa2a40d6d7bf9d8a573d5eef6d6
Javier Mendonça <javier.jmc@gmail.com>
2017-11-13 08:03:37 +0100

    Injecting dependencies on base fragment.

--- app/src/main/java/com/example/themoviedb/dagger2/TheMovieDbAppComponent.java
+++ app/src/main/java/com/example/themoviedb/dagger2/TheMovieDbAppComponent.java
@@ -2,7 +2,10 @@ package com.example.themoviedb.dagger2;
 
 
 import com.example.themoviedb.TheMovieDbApp;
+import com.example.themoviedb.dagger2.modules.ApiModule;
+import com.example.themoviedb.dagger2.modules.TheMovieDbActivitiesModule;
 import com.example.themoviedb.dagger2.modules.TheMovieDbAppModule;
+import com.example.themoviedb.dagger2.modules.TheMovieDbRepoBindsModule;
 import com.example.themoviedb.dagger2.scopes.ApplicationScope;
 
 import dagger.Component;
@@ -11,7 +14,10 @@ import dagger.android.AndroidInjector;
 @ApplicationScope
 @Component(
     modules = {
-        TheMovieDbAppModule.class
+        TheMovieDbAppModule.class,
+        TheMovieDbActivitiesModule.class,
+        ApiModule.class,
+        TheMovieDbRepoBindsModule.class
     })
 public interface TheMovieDbAppComponent extends AndroidInjector<TheMovieDbApp> {
 
