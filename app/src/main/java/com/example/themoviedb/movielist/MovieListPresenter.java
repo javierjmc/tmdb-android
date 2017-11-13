7da3fe028454803329015dfc5c6dcea095578960
Javier Mendonça <javier.jmc@gmail.com>
2017-11-13 08:06:31 +0100

    Removing dummy items from presenter.

--- app/src/main/java/com/example/themoviedb/movielist/MovieListPresenter.java
+++ app/src/main/java/com/example/themoviedb/movielist/MovieListPresenter.java
@@ -1,62 +1,32 @@
 package com.example.themoviedb.movielist;
 
-import com.example.themoviedb.data.model.Movie;
+import com.example.themoviedb.data.domain.MoviesRepo;
 import com.hannesdorfmann.mosby3.mvi.MviBasePresenter;
 
-import org.joda.time.DateTime;
-
-import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
+
+import javax.inject.Inject;
 
 import io.reactivex.Observable;
 
 public class MovieListPresenter extends MviBasePresenter<MovieListView, MovieListViewState> {
 
+    private final MoviesRepo moviesRepo;
+
+    @Inject
+    public MovieListPresenter(final MoviesRepo moviesRepo) {
+        this.moviesRepo = moviesRepo;
+    }
+
     private final List emptyList = Collections.EMPTY_LIST;
-    private final List dummy = new ArrayList<Movie>() {{
-        add(Movie.builder()
-            .id(0)
-            .voteAverage(7.5f)
-            .voteCount(10)
-            .title("It")
-            .posterPath(null)
-            .originalLanguage(null)
-            .originalTitle(null)
-            .genreIds(new ArrayList<Long>() {{
-                add(1L);
-                add(2L);
-            }})
-            .adult(false)
-            .overview("Movie's overview")
-            .releaseDate(DateTime.now())
-            .build());
-
-        add(Movie.builder()
-            .id(1)
-            .voteAverage(8f)
-            .voteCount(10)
-            .title("It 2")
-            .posterPath(null)
-            .originalLanguage(null)
-            .originalTitle(null)
-            .genreIds(new ArrayList<Long>() {{
-                add(1L);
-                add(2L);
-            }})
-            .adult(false)
-            .overview("Movie's overview 2")
-            .releaseDate(DateTime.now())
-            .build());
-
-    }};
 
     @Override
     protected void bindIntents() {
 
         Observable<MovieListViewState> loadMovies = intent(MovieListView::loadMoviesFirstPageIntent)
-            .flatMap(ignored -> Observable.just(dummy)
-                .map(items -> new MovieListViewState(false, null, items, false, null))
+            .flatMap(ignored -> moviesRepo.getMostPopularMovies(1)
+                .map(movies -> new MovieListViewState(false, null, movies, false, null))
                 .startWith(new MovieListViewState(true, null, emptyList, false, null))
                 .onErrorReturn(error -> new MovieListViewState(false, (Throwable) error, emptyList, false, null)));
 
